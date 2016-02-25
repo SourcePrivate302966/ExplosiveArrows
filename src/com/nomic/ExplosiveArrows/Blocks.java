1d5320cbf8621d76cd79aa6ffc3f2ecf41100ba0
Braden Mitchell <braden.jr.mitch@gmail.com>
2016-02-24 23:01:49 -0800

    Version 1.3.0

--- src/com/nomic/ExplosiveArrows/Blocks.java
+++ src/com/nomic/ExplosiveArrows/Blocks.java
@@ -8,27 +8,29 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 
-public class Blocks implements Listener{
-	
-	private Main plugin;
+public class Blocks implements Listener {
 
+	private Main plugin;
 	public Blocks(Main pl) {
 		plugin = pl;
 	}
-	
+
 	@EventHandler
 	public void onArrowHit(ProjectileHitEvent e) {
 		if (!(plugin.getConfig().getDefaultSection().get("blockExplosions").equals(true)))
+			return;
+		Player p = (Player) e.getEntity().getShooter();
+		Location l = e.getEntity().getLocation();
+		World w = l.getWorld();
+		
+		if (!(Main.getWorldGuard().canBuild(p, l)))
 			return;
 		if (!(e.getEntity().getShooter() instanceof Player))
 			return;
 		if (!(e.getEntity().getType() == EntityType.ARROW))
 			return;
-		Player p = (Player) e.getEntity().getShooter();
 		if (!(p.hasPermission("arrow.explosive") || p.isOp()))
 			return;
-		Location l = e.getEntity().getLocation();
-		World w = l.getWorld();
 		w.createExplosion(l, plugin.getConfig().getInt("explosionPower"));
 		e.getEntity().remove();
 	}
--- src/com/nomic/ExplosiveArrows/Main.java
+++ src/com/nomic/ExplosiveArrows/Main.java
@@ -1,7 +1,11 @@
 package com.nomic.ExplosiveArrows;
 
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 
 public class Main extends JavaPlugin {
 
@@ -14,5 +18,13 @@ public class Main extends JavaPlugin {
 
 		pm.registerEvents(new Blocks(this), this);
 		pm.registerEvents(new Players(this), this);
+	}
+	
+	public static WorldGuardPlugin getWorldGuard() {
+		Plugin wg = Bukkit.getServer().getPluginManager().getPlugin("WorldGuard");
+		if ((wg != null) && ((wg instanceof WorldGuardPlugin)) && (wg.isEnabled())) {
+			return (WorldGuardPlugin) wg;
+		}
+		return null;
 	}
 }
